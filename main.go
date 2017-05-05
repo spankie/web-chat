af7f6dcfde5ce683a6fbce460f09693234ab7f9c
spankie <odohi.david@gmail.com>
2017-05-05 18:43:32 +0100

    Initial Setup

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+	"github.com/rs/cors"
+	"github.com/spankie/web-chat/web"
+)
+
+const (
+	// DEFAULTPORT holds the default port for the server
+	DEFAULTPORT = "8080"
+)
+
+// Router struct would carry the httprouter instance, so its methods could be verwritten and replaced with methds with wraphandler
+type Router struct {
+	*httprouter.Router
+}
+
+// Get is an endpoint to only accept requests of method GET
+func (r *Router) Get(path string, handler http.Handler) {
+	r.GET(path, wrapHandler(handler))
+}
+
+// Post is an endpoint to only accept requests of method POST
+func (r *Router) Post(path string, handler http.Handler) {
+	r.POST(path, wrapHandler(handler))
+}
+
+// Put is an endpoint to only accept requests of method PUT
+func (r *Router) Put(path string, handler http.Handler) {
+	r.PUT(path, wrapHandler(handler))
+}
+
+// Delete is an endpoint to only accept requests of method DELETE
+func (r *Router) Delete(path string, handler http.Handler) {
+	r.DELETE(path, wrapHandler(handler))
+}
+
+// NewRouter is a wrapper that makes the httprouter struct a child of the router struct
+func NewRouter() *Router {
+	return &Router{httprouter.New()}
+}
+
+func wrapHandler(h http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		r = r.WithContext(ctx)
+		//r.Context().Set(r, "params", ps)
+		//r.Context().
+		//context.Set(r, "params", ps)
+		h.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	log.Println("Hello Web Chat")
+
+	// Initialize the http router ...
+	router := NewRouter()
+
+	commonHandlers := alice.New(web.LoggingHandler)
+
+	router.Get("/", commonHandlers.ThenFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "web/templates/index.html")
+	}))
+
+	// SERVE STATIC FILES FROM THE STATIC FOLDER
+	fileServer := http.FileServer(http.Dir("./web/static"))
+	router.GET("/assets/*filepath", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("Vary", "Accept-Encoding")
+		w.Header().Set("Cache-Control", "public, max-age=7776000")
+		r.URL.Path = p.ByName("filepath")
+		fileServer.ServeHTTP(w, r)
+	})
+
+	// Get the port to serve on from environment variable
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Println("No Global port has been defined, using default 8080")
+		PORT = "8080"
+	}
+
+	// This handler overrides the default servemux
+	handler := cors.New(cors.Options{
+		//		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Accept", "Content-Type", "X-Auth-Token", "*"},
+		Debug:            false,
+	}).Handler(router)
+
+	log.Println("serving on port :", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
+}
---
+++ web/middlewares.go
@@ -0,0 +1,19 @@
+package web
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+//LoggingHandler Logs request time, method and duration of handler/request execution
+func LoggingHandler(next http.Handler) http.Handler {
+	logger := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		responseTime := time.Since(t1)
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), responseTime)
+	}
+
+	return http.HandlerFunc(logger)
+}
